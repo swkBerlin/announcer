47db534f7baf20c315601eff0a12bb390a488023
Egga (Jan Hartung) <github@egga.de>
2015-02-01 22:18:26 +0100

    read api key from env

--- src/main/java/de/swk/announcer/infrastructure/Configuration.java
+++ src/main/java/de/swk/announcer/infrastructure/Configuration.java
@@ -1,7 +1,5 @@
 package de.swk.announcer.infrastructure;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -9,31 +7,21 @@ import java.util.Properties;
  */
 public class Configuration {
 
-    public static final String CONFIG_LOCATION = "/home/egga/announcer.config";
-    public static final String MEETUP_COM_API_KEY = "meetup_key";
+    public static final String MEETUP_COM_API_KEY = "MEETUP_COM_API_KEY";
 
     final Properties values = new Properties();
 
     public Configuration() {
-        readConfiguration();
+        init();
     }
 
     public String getMeetUpComApiKey() {
         return values.getProperty(MEETUP_COM_API_KEY);
     }
 
-    private void readConfiguration() {
-        try {
-            readConfigurationFromFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void init() {
+        String getenv = System.getenv(MEETUP_COM_API_KEY);
+        values.put(MEETUP_COM_API_KEY, getenv);
     }
 
-    private Properties readConfigurationFromFile() throws IOException {
-        FileInputStream file = new FileInputStream(CONFIG_LOCATION);
-        values.load(file);
-        file.close();
-        return values;
-    }
 }
